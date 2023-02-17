40f5b7a502acf9dd173755c7dc1d5ac2a58b0282
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-02-17 23:22:06 +0200

    video player set

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+    
+
+    const player = new Player(iframe)
+
+
+
+const onPlay = function(data) {
+    console.log(data)
+};
+
+player.on('play', onPlay);
