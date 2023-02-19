72b3875e5bd724ba7042df32ae500b398219141d
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-02-19 13:07:08 +0200

    task 3 continion work

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,19 +3,38 @@ import throttle from "lodash.throttle";
 const form = document.querySelector('.feedback-form');
 
 
+const takeValues = () => {
+    // const { elements: {
+    //     email,
+    //     message }, } = form;
+     const currentValue = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    }
+    return currentValue
+}
 
-const onFormClick = () => { 
-    const mailVal = form.elements.email.value;
-const messageVal = form.elements.message.value;
+
+
+
+const onFormClick = () => {
+    //     const mailVal = form.elements.email.value;
+    // const messageVal = form.elements.message.value;
     // console.log(form.elements.email.value);
     // console.log(form.elements.message.value);
-    const currentValues = {
-        email: mailVal,
-        message: messageVal,
-    }
-    // console.log(currentValues)
-    localStorage.setItem('feedback-form-state', JSON.stringify(currentValues));
-    
+   
+        // console.log(email, message)
+
+   
+
+    // console.log(email, message)
+    // takeValues()
+
+    const createForm = takeValues();
+     localStorage.setItem('feedback-form-state', JSON.stringify(createForm));
+
+
+    // console.log(takeValues()); 
 };
 
 form.addEventListener('input', onFormClick);
@@ -23,12 +42,19 @@ form.addEventListener('input', onFormClick);
 // console.log(localStorage.getItem('feedback-form-state'))
 
 
-const onSubmit = () => {
-    // if (localStorage.getItem('feedback-form-state'))
+const onSubmit = (evt) => {
+    evt.preventDefault()
+    if (localStorage.getItem('feedback-form-state')) {
 
-    const savedValues = localStorage.getItem("feedback-form-state");
-const parsedValues = JSON.parse(savedValues);
-// console.log(parsedValues); 
+        const savedValues = localStorage.getItem("feedback-form-state") || '';
+        const parsedValues = JSON.parse(savedValues);
+        console.log(parsedValues);
+    }
+    {
+        form.elements.email.value = '';
+        form.elements.message.value = '';
+    }
+    form.reset();
 }
 
 window.addEventListener('submit', onSubmit);
