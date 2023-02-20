95e4af5a687ffbab6c386169f4fdd7ce84c42d48
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-02-20 12:55:54 +0200

    task 3 refresh

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,21 @@
 import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
-// import  throttle  from 'lodash.throttle';
+
+const STORAGE_KEY = 'videoplayer - current - time';
 
 const iframe = document.querySelector('iframe');
-    
 
-    const player = new Player(iframe)
-
-
+const player = new Player(iframe);
 
 const onPlay = function (data) {
-    localStorage.setItem('videoplayer - current - time', data.seconds)
-    console.log(localStorage);
-}
-
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+  console.log(localStorage);
+};
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer - current - time'))
-
-
-
-
+try {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || '');
+} catch (error) {
+  console.error('Get state error: ', error.message);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,61 +1,48 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-
-const STORAGE_KEY = 'feedback-form-state'
+const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 
-onLoadingPage()
+// Виклик функції яка буде заповнювати поля форми даними зі  сховища(якщо вони є) під час перезавантаження сторінки або шнших проблем
 
+onLoadingPage();
 
-
-// const takeValues = () => {
-//     // const { elements: {
-//     //     email,
-//     //     message }, } = form;
-//      const currentValue = {
-//         email: form.elements.email.value,
-//         message: form.elements.message.value,
-//     }
-//     return currentValue
-// }
+// Створення обєкту в який буде записуватися вміст форми
 
 const currentValue = {};
 
+// Опис функції яка буде збирати дані з форми та записувати їх в локальне сховище
 
-const onFormClick = (evt) => {
-   
+const onFormClick = evt => {
+  currentValue[evt.target.name] = evt.target.value;
 
-    currentValue[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(currentValue));
+};
 
-   
-     localStorage.setItem(STORAGE_KEY, JSON.stringify(currentValue));
+form.addEventListener('input', throttle(onFormClick, 500));
 
+// Опис функції, яка при відправлені форми очищує її та виводить дані в консоль
 
-    };
+const onSubmit = evt => {
+  evt.preventDefault();
 
-form.addEventListener('input', throttle(onFormClick,500));
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(currentValue);
+};
 
-
-
-const onSubmit = (evt) => {
-    evt.preventDefault()
-       
-    form.reset();
-    localStorage.removeItem(STORAGE_KEY);
-    console.log(currentValue)
-    }
-
-window.addEventListener('submit', onSubmit);
-
+form.addEventListener('submit', onSubmit);
 
 function onLoadingPage() {
-    const savedFormData = localStorage.getItem(STORAGE_KEY) || '';
-    if (savedFormData) {
-        
-    const parsedValues = JSON.parse(savedFormData);
-       form.elements.email.value = parsedValues.email;
-        form.elements.message.value = parsedValues.message;
+  const savedFormData = localStorage.getItem(STORAGE_KEY) || '';
+  if (savedFormData) {
+    try {
+      const parsedValues = JSON.parse(savedFormData);
+      form.elements.email.value = parsedValues.email;
+      form.elements.message.value = parsedValues.message;
+    } catch (error) {
+      console.error('Get state error: ', error.message);
     }
-    
+  }
 }
